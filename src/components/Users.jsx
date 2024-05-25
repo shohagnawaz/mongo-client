1657d1eecb717951086e9ac5ff85822a85404645
shohagnawaz <snawazdigital@gmail.com>
2024-05-25 19:26:40 +0600

    rc

---
+++ src/components/Users.jsx
@@ -0,0 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+    const users = useLoaderData();
+    return (
+        <div>
+            <h2>{users.length}</h2>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
